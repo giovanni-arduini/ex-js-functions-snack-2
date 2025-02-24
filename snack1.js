4c64f429aaaaa0ff1a5fba365f1f68c2a775dfc0
giovanni-arduini <gionnibigudd@gmail.com>
2025-02-24 10:57:06 +0100

    ADD snack 1 solution

---
+++ snack1.js
@@ -0,0 +1,19 @@
+function somma(n1, n2){
+    return n1 + n2
+}
+
+console.log(somma(2,4))
+
+//
+
+const sommaDiDueNumeri = function(n1, n2){
+    return n1 + n2
+}
+
+console.log(sommaDiDueNumeri(2,5))
+
+//
+
+const sommaArrow = (n1, n2)=> n1 + n2;
+
+console.log(sommaArrow(2,6))
